23ef8ffc32c6e55c6858fe56661b9442defe82c5
joseeduardo0131gmailcom <joseeduardo0131@gmail.com>
2021-02-19 12:41:28 -0300

    projeto final

---
+++ Classes/Serie.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dio.Series
+{
+    public class Serie : EntidadeBase
+    {
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descricao: " + this.Descricao + Environment.NewLine;
+            retorno += "Início da série: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public bool Excluir()
+        {
+            return this.Excluido = true;
+        }
+    }
+}
